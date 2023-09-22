c87ab8f16b01bc629daa444ecdd64874ca4648d7
ApurvaParanjape <apurvaparanjape27@gmail.com>
2023-09-22 11:47:01 +0530

    Till here: creating/ registering a user

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import morgan from "morgan";
+import {register} from "./controllers/auth.js"
 
 //CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({storage})
+
+//Routes with files
+app.post("/auth/register", upload.single("picture"), register)
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001
